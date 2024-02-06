7672ee0b42e5e74821da4bd3e921c5f7bad6dcb2
StefanHolz1997 <Stefanholz97@web.de>
2024-02-06 12:20:36 +0100

    1_3 done

---
+++ 2024_Februar/06.02.24-numbers_math-methods/Übungen/MathLevel-1_3/assets/js/main.js
@@ -0,0 +1,7 @@
+let randomNum = "";
+let randomNum10 = "";
+let randomNum100 = "";
+
+console.log(Math.random(randomNum));
+console.log(Math.ceil(Math.random(randomNum10) * 10));
+console.log(Math.ceil(Math.random(randomNum100) * 100));
