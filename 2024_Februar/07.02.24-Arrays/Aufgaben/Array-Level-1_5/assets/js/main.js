e565a3496ac1aef4cd92ecef7201ded96c708689
StefanHolz1997 <Stefanholz97@web.de>
2024-02-07 13:11:58 +0100

    1.5_done

---
+++ 2024_Februar/07.02.24-Arrays/Aufgaben/Array-Level-1_5/assets/js/main.js
@@ -0,0 +1,8 @@
+const games = ["Genshin", "Valorant", "Palworld", "DayZ"];
+games.push("Secret of Mana");
+games.push("Hollow Knight");
+console.log(games);
+
+let gameGone = games.pop();
+console.log(gameGone);
+console.log(games);
