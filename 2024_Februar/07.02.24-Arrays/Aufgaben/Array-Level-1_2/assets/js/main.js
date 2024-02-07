f281d7544121e2bfc317aaf766d9a7ef81783536
StefanHolz1997 <Stefanholz97@web.de>
2024-02-07 13:04:01 +0100

    1.2_Done

---
+++ 2024_Februar/07.02.24-Arrays/Aufgaben/Array-Level-1_2/assets/js/main.js
@@ -0,0 +1,7 @@
+const person = ["Gaming", "Dog", "Camera"];
+const friends = ["Lea", "Sven", "Araffi"];
+const favoriteFood = ["Ramen", "Rice", "Pizza"];
+
+console.log(person[1]);
+console.log(friends[0]);
+console.log(favoriteFood[2]);
