ffd6341f82a6ec7101917b2421cbb44e5063c215
StefanHolz1997 <Stefanholz97@web.de>
2024-02-07 13:05:42 +0100

    1.3_Done

---
+++ 2024_Februar/07.02.24-Arrays/Aufgaben/Array-Level-1_3/assets/js/main.js
@@ -0,0 +1,7 @@
+const person = ["Gaming", "Dog", "Camera"];
+const friends = ["Lea", "Sven", "Araffi"];
+const favoriteFood = ["Ramen", "Rice", "Pizza"];
+
+console.log(person.length);
+console.log(friends.length);
+console.log(favoriteFood.length);
