8345087d7f5559ad3598ea9277571c32a09f666d
StefanHolz1997 <Stefanholz97@web.de>
2024-02-07 13:14:46 +0100

    1.7_Done

---
+++ 2024_Februar/07.02.24-Arrays/Aufgaben/Array-Level-1_7/assets/js/main.js
@@ -0,0 +1,10 @@
+const person = ["Gaming", "Dog", "Camera"];
+const friends = ["Lea", "Sven", "Araffi"];
+const favoriteFood = ["Ramen", "Rice", "Pizza"];
+
+friends.unshift("Tobi");
+friends.unshift("Vanessa");
+console.log(friends);
+favoriteFood.unshift("Kaki", "Himbeeren");
+
+console.log(favoriteFood);
