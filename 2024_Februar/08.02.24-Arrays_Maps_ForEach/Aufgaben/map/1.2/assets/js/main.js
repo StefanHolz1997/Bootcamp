ff706c13973097c476391f306cf2bfce449e8a54
StefanHolz1997 <Stefanholz97@web.de>
2024-02-08 15:58:02 +0100

    Done

---
+++ 2024_Februar/08.02.24-Arrays_Maps_ForEach/Aufgaben/map/1.2/assets/js/main.js
@@ -0,0 +1,14 @@
+const drinks = [
+  "Coca-Cola",
+  "Apfelsaft",
+  "Pepsi",
+  "Traubensaft",
+  "Sprite",
+  "Orangensaft",
+  "Red Bull Energy Drink",
+  "Fanta",
+];
+
+const upperFunction = drinks.map((getränke) => getränke.toUpperCase());
+
+console.log(upperFunction);
