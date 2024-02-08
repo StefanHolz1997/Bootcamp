39d2c4037d042346b69bb654f270ec4f8495f927
StefanHolz1997 <Stefanholz97@web.de>
2024-02-08 15:25:18 +0100

    sort done

---
+++ 2024_Februar/08.02.24-Arrays_Maps_ForEach/Aufgaben/sort/1.2/assets/js/main.js
@@ -0,0 +1,21 @@
+let languages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "Ruby",
+  "PHP",
+  "C++",
+  "CSS",
+  "C#",
+  "Go",
+  "C",
+  "TypeScript",
+  "Swift",
+];
+
+function sortierung2() {
+  languages.sort();
+  languages.reverse();
+  console.log(languages);
+}
+sortierung2();
