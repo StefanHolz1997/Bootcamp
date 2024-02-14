dc90c582d172799c6e869dc760b177ecdc0ea990
StefanHolz1997 <Stefanholz97@web.de>
2024-02-14 11:15:56 +0100

    Aufgabe 1

---
+++ 2024_Februar/14.02.24-Eventlistener/Aufgaben/1_3/assets/js/main.js
@@ -0,0 +1,7 @@
+const cardCustom = document.querySelector(".card");
+
+function changeColor(event) {
+  const input = event.target;
+  input.style.backgroundColor = "black";
+  input.style.color = "white";
+}
