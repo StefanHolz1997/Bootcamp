23a4d32d16c842fafba818921cdef6099545c5b5
StefanHolz1997 <Stefanholz97@web.de>
2024-02-19 13:37:47 +0100

    BeautyChange

--- 2024_Februar/19.02.24-Date/Aufgaben/2_1/assets/js/main.js
+++ 2024_Februar/19.02.24-Date/Aufgaben/2_1/assets/js/main.js
@@ -1,7 +1,6 @@
 const dateOutput = document.querySelector(".date");
 const currentDate = () => {
   let today = new Date();
-  let weekDayName = today.toLocaleString("default", { weekday: "short" });
   dateOutput.innerHTML = `<h2 style="font-family: Verdana"> ${today.getDate()} / ${today.getMonth()} / ${today.getFullYear()} <p>`;
 };
 
