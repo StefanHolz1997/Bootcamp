29870849e1f4c0c2d6671c181bda245b14e3fad7
StefanHolz1997 <Stefanholz97@web.de>
2024-02-20 14:16:36 +0100

    1_3 Done

---
+++ 2024_Februar/20.02.24/Aufgaben/1_3/assets/js/main.js
@@ -0,0 +1,12 @@
+console.log(window.navigator);
+
+console.log(window.navigator.appCodeName);
+console.log(window.navigator.userAgent);
+console.log(window.navigator.appVersion);
+console.log(window.navigator.platform);
+
+console.log(window.screen.width);
+console.log(window.screen.height);
+
+console.log(window.screen.colorDepth);
+console.log(window.screen.pixelDepth);
