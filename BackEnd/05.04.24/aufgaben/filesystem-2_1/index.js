2ac68b97727894b95e5c44b07d8de8314c303341
StefanHolz1997 <Stefanholz97@web.de>
2024-04-05 15:18:57 +0200

    2.1 Done

---
+++ BackEnd/05.04.24/aufgaben/filesystem-2_1/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const data = require("./data.json");
+
+data.map((item) => {
+  fs.appendFile(
+    "./data.txt",
+    `${item.id} - ${item.title} \n${item.description} \n \n`,
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+});
